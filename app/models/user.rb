a86567d6227388f5c5b6309eaa374af8d59fadf1
Jordan Epps <jordan.k.epps@gmail.com>
2015-11-17 10:55:35 -0800

    User model created.

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	before_create :generate_auth_token
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :username, length: { minimum: 1, maximum: 20 }, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 100 },
+            format: { with: EMAIL_REGEX }
+
+  has_secure_password
+
+	private
+	def generate_auth_token
+    loop do
+      self.auth_token = SecureRandom.base64(64)
+      break unless User.find_by(auth_token: auth_token)
+    end
+  end
+end
