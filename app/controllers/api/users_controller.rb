de54f6e25f1d970965a6c60cb247e2d2374d6ee0
Jordan Epps <jordan.k.epps@gmail.com>
2015-12-07 17:39:17 -0800

    added #show to users_controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,6 +6,11 @@ class Api::UsersController < ApiController
 		render json: users, each_serializer: UserSerializer, status: 200
 	end
 
+	def show
+		user = User.find(params[:id])
+		render json: user
+	end
+
 	def create
 		user = User.new(user_params)
 		if user.save
